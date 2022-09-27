98c6fe3b29116b8f8da9207d30abc5a52b2127b6
эдиль <edu007super@gmail.com>
2022-09-27 22:33:24 +0600

    add burger-menu

--- js/main.js
+++ js/main.js
@@ -48,3 +48,23 @@
     };
     scrollTo();
 }());
+//burger headler
+(function () {
+	const burgerItem = document.querySelector('.burger');
+	const menu = document.querySelector('.header__nav');
+	const menuCloseItem = document.querySelector('.header__nav-close');
+    const menuLinks = document.querySelectorAll('.header__link');
+	burgerItem.addEventListener('click', () => {
+		menu.classList.add('header_nav_active');
+	});
+	menuCloseItem.addEventListener('click', () =>{
+		menu.classList.remove('header_nav_active');
+	});
+    if (window.innerWidth <= 767) {
+        for (let i = 0; i < menuLinks.length; i += 1) {
+            menuLinks[i].addEventListener('click', () => {
+                menu.classList.remove('header_nav_active');
+            });
+        }
+    }
+}());
